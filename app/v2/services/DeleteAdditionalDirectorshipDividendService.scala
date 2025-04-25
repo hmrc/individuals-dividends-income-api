a654c90a74afeee1a79faad29b6bb3f4fbbb9384
“Taiwo-AB” <63714619+Taiwo-AB@users.noreply.github.com>
2025-04-25 10:08:05 +0100

    MTDSA-29757 Fix Delete and Retrieve ADD, add model tests and use Timestamp for submittedOn field

---
+++ app/v2/services/DeleteAdditionalDirectorshipDividendService.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.services
+
+import cats.implicits._
+import shared.controllers.RequestContext
+import shared.models.errors._
+import shared.services.{BaseService, ServiceOutcome}
+import v2.connectors.DeleteAdditionalDirectorshipDividendConnector
+import v2.models.request.deleteAdditionalDirectorshipDividend.DeleteAdditionalDirectorshipDividendRequest
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class DeleteAdditionalDirectorshipDividendService @Inject()(connector: DeleteAdditionalDirectorshipDividendConnector) extends BaseService {
+
+  def delete(request: DeleteAdditionalDirectorshipDividendRequest)(implicit ctx: RequestContext, ec: ExecutionContext): Future[ServiceOutcome[Unit]] =
+    connector.delete(request).map(_.leftMap(mapDownstreamErrors(downstreamErrorMap)))
+
+  private val downstreamErrorMap: Map[String, MtdError] = Map(
+    "1215" -> NinoFormatError,
+    "1117" -> TaxYearFormatError,
+    "1217" -> EmploymentIdFormatError,
+    "1216" -> InternalError,
+    "5010" -> NotFoundError
+  )
+
+}
