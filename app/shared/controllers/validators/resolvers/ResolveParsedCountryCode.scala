7372cd26882fb31af3d210beac2a2eba783ae78d
muhammadmulla1 <muhammad_mulla@icloud.com>
2026-04-22 18:21:00 +0100

    MTDSA-34041: Align resolver and tests with POC implementation

--- app/shared/controllers/validators/resolvers/ResolveParsedCountryCode.scala
+++ app/shared/controllers/validators/resolvers/ResolveParsedCountryCode.scala
@@ -1,23 +1,8 @@
-/*
- * Copyright 2023 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package shared.controllers.validators.resolvers
 
 import cats.data.Validated
 import cats.data.Validated.{Invalid, Valid}
+import com.neovisionaries.i18n.CountryCode
 import shared.models.errors.{CountryCodeFormatError, MtdError, RuleCountryCodeError}
 
 case class ResolveParsedCountryCode(path: String) {
@@ -25,10 +10,18 @@ case class ResolveParsedCountryCode(path: String) {
   def apply(value: String): Validated[List[MtdError], String] = {
     if (value.length != 3) {
       Invalid(List(CountryCodeFormatError.withPath(path)))
+
+    } else if (ResolveParsedCountryCode.permittedCustomCodes.contains(value)) {
+      Valid(value)
+
     } else if (ResolveParsedCountryCode.permittedCodes.contains(value)) {
       Valid(value)
+
     } else {
-      Invalid(List(RuleCountryCodeError.withPath(path)))
+      Option(CountryCode.getByAlpha3Code(value)) match {
+        case Some(_) => Valid(value)
+        case None    => Invalid(List(RuleCountryCodeError.withPath(path)))
+      }
     }
   }
 
@@ -41,6 +34,8 @@ case class ResolveParsedCountryCode(path: String) {
 }
 
 object ResolveParsedCountryCode {
+
+  private val permittedCustomCodes = Set("ZZZ")
 
   val permittedCodes: Set[String] = Set(
     "AFG","ALB","DZA","ASM","AND","AGO","AIA","ATG","ARG","ARM","ABW","AUS","AUT","AZE",
